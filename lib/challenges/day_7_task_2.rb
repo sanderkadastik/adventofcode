7496a1d6bfdbbda9f46174d92068433c976dfdbc
Sander Kadastik <56273204+sanderkadastik@users.noreply.github.com>
2022-12-28 10:19:49 +0200

    Update day_7_task_2.rb

--- lib/challenges/day_7_task_2.rb
+++ lib/challenges/day_7_task_2.rb
@@ -150,7 +150,6 @@ class FileSystem
     currently_used = @directories["/"].final_size
     empty_space_on_disk = disk_size - currently_used
     needs_to_be_deleted = needed_size - empty_space_on_disk
-    p needs_to_be_deleted
     
     current_candidate = @directories["/"].final_size
     
